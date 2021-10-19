f364cf11ac493393550a5dea5fef168433197025
Antonio-Ionaton <antem@bu.edu>
2021-10-18 21:35:53 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gradientcorrection",
+    version="0.1",
+    description="Gradient correction method",
+    author="Antonio Majdandzic",
+    author_email="antem@bu.edu",
+    platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    url="https://github.com/Antonio-Ionaton/GradientCorrection",
+    packages=find_packages(),
+)
