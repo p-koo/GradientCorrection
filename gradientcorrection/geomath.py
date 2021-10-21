32095e243c1b6f7ad2f273d75534af0589e4255a
Antonio-Ionaton <antem@bu.edu>
2021-10-21 10:40:16 -0400

    Update geomath.py

--- gradientcorrection/geomath.py
+++ gradientcorrection/geomath.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def Scalar_product(attr_score_copy, X_model_normalized_copy):
-	scalar_product = np.average ( np.sum(np.sum(np.multiply(attr_score_copy , X_model_normalized_copy), axis=2), axis=1)/(   np.sqrt(np.sum(np.sum(np.multiply(attr_score_copy,attr_score_copy), axis=2), axis=1) )  * np.sqrt(np.sum(np.sum(np.multiply(X_model_normalized_copy,X_model_normalized_copy), axis=2), axis=1) ) )  )
+def Scalar_product(x1, x2):
+	scalar_product = np.average ( np.sum(np.sum(np.multiply(x1 , x2), axis=2), axis=1)/(   np.sqrt(np.sum(np.sum(np.multiply(x1,x1), axis=2), axis=1) )  * np.sqrt(np.sum(np.sum(np.multiply(x2,x2), axis=2), axis=1) ) )  )
 	return scalar_product
 
-def Scalar_product_set(attr_score_copy, X_model_normalized_copy):
-	scalar_product = np.sum(np.sum(np.multiply(attr_score_copy , X_model_normalized_copy), axis=2), axis=1)/(   np.sqrt(np.sum(np.sum(np.multiply(attr_score_copy,attr_score_copy), axis=2), axis=1) )  * np.sqrt(np.sum(np.sum(np.multiply(X_model_normalized_copy,X_model_normalized_copy), axis=2), axis=1) ) )  
+def Scalar_product_set(x1, x2):
+	scalar_product = np.sum(np.sum(np.multiply(x1 , x2), axis=2), axis=1)/(   np.sqrt(np.sum(np.sum(np.multiply(x1,x1), axis=2), axis=1) )  * np.sqrt(np.sum(np.sum(np.multiply(x2,x2), axis=2), axis=1) ) )  
 	return scalar_product   
 
 
