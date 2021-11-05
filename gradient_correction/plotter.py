9086a43d12658df110f884e8ea05b35c6550143f
Antonio-Ionaton <antem@bu.edu>
2021-11-05 13:38:10 -0400

    Update plotter.py

--- gradient_correction/plotter.py
+++ gradient_correction/plotter.py
@@ -12,20 +12,20 @@ def plot_improvement(attribution, attribution_corrected, x_min, x_max, y_min, y_
     ax1.add_patch(patches.Rectangle((x_min, 0), 1.3, y_min, facecolor = 'red', fill=True , alpha=0.1))
 
     #Cosine
-    ax1.scatter(attribution['cnn-local_relu'], attribution_corrected['cnn-local_relu']-attribution['cnn-local_relu'], s=s, edgecolors='g',facecolors='none', marker="o", alpha=alpha) #label='COSINE'
-    mean1a=np.average(attribution_corrected['cnn-local_relu']-attribution['cnn-local_relu'])
+    ax1.scatter(attribution['shallow_relu'], attribution_corrected['shallow_relu']-attribution['shallow_relu'], s=s, edgecolors='g',facecolors='none', marker="o", alpha=alpha) #label='COSINE'
+    mean1a=np.average(attribution_corrected['shallow_relu']-attribution['shallow_relu'])
 
     #Cosine
-    ax1.scatter( attribution['cnn-local_exponential'], attribution_corrected['cnn-local_exponential']-attribution['cnn-local_exponential'], s=s, edgecolors='r',facecolors='none', marker="o", alpha=alpha)
-    mean1b=np.average(attribution_corrected['cnn-local_exponential']-attribution['cnn-local_exponential'])
+    ax1.scatter( attribution['shallow_exp'], attribution_corrected['shallow_exp']-attribution['shallow_exp'], s=s, edgecolors='r',facecolors='none', marker="o", alpha=alpha)
+    mean1b=np.average(attribution_corrected['shallow_exp']-attribution['shallow_exp'])
 
     #Cosine
-    ax1.scatter( attribution['cnn-dist_relu'], attribution_corrected['cnn-dist_relu']-attribution['cnn-dist_relu'], s=s, edgecolors='b',facecolors='none', marker="o", alpha=alpha)
-    mean1c=np.average(np.nan_to_num(attribution_corrected['cnn-dist_relu']-attribution['cnn-dist_relu']))
+    ax1.scatter( attribution['deep_relu'], attribution_corrected['deep_relu']-attribution['deep_relu'], s=s, edgecolors='b',facecolors='none', marker="o", alpha=alpha)
+    mean1c=np.average(np.nan_to_num(attribution_corrected['deep_relu']-attribution['deep_relu']))
 
     #Cosine
-    ax1.scatter( attribution['cnn-dist_exponential'], attribution_corrected['cnn-dist_exponential']-attribution['cnn-dist_exponential'], s=s, edgecolors='black',facecolors='none', marker="o", alpha=alpha)
-    mean1d=np.average(np.nan_to_num(attribution_corrected['cnn-dist_exponential']-attribution['cnn-dist_exponential']))
+    ax1.scatter( attribution['deep_exp'], attribution_corrected['deep_exp']-attribution['deep_exp'], s=s, edgecolors='black',facecolors='none', marker="o", alpha=alpha)
+    mean1d=np.average(np.nan_to_num(attribution_corrected['deep_exp']-attribution['deep_exp']))
 
     x_ = np.linspace(x_min, 1.0, 100)
     ax1.plot(x_, x_*0, c="black");
@@ -47,19 +47,19 @@ def plot_attribution_vs_performance(attribution, performance, labelsize=15, font
     alpha = 0.9
     #---------------------
     #Cosine
-    ax1.scatter( attribution['cnn-local_relu'], performance['cnn-local_relu'], s=s, edgecolors='g',facecolors='none', marker="o", alpha=alpha) #label='COSINE'
+    ax1.scatter( attribution['shallow_relu'], performance['shallow_relu'], s=s, edgecolors='g',facecolors='none', marker="o", alpha=alpha) #label='COSINE'
 
     #---------------------
     #Cosine
-    ax1.scatter( attribution['cnn-local_exponential'], performance['cnn-local_exponential'], s=s, edgecolors='r',facecolors='none', marker="o", alpha=alpha)
+    ax1.scatter( attribution['shallow_exp'], performance['shallow_exp'], s=s, edgecolors='r',facecolors='none', marker="o", alpha=alpha)
 
     #---------------------
     #Cosine
-    ax1.scatter( attribution['cnn-dist_relu'], performance['cnn-dist_relu'], s=s, edgecolors='b',facecolors='none', marker="o", alpha=alpha)
+    ax1.scatter( attribution['deep_relu'], performance['deep_relu'], s=s, edgecolors='b',facecolors='none', marker="o", alpha=alpha)
 
     #---------------------
     #Cosine
-    ax1.scatter( attribution['cnn-dist_exponential'], performance['cnn-dist_exponential'], s=s, edgecolors='black',facecolors='none', marker="o", alpha=alpha)
+    ax1.scatter( attribution['deep_exp'], performance['deep_exp'], s=s, edgecolors='black',facecolors='none', marker="o", alpha=alpha)
 
     ax1.tick_params(axis="x", labelsize=labelsize)
     ax1.tick_params(axis="y", labelsize=labelsize) 
