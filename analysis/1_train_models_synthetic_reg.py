e058df89b6b35b7240a4b74b681e0bdb5fb9d0ba
Antonio-Ionaton <antem@bu.edu>
2021-12-06 02:40:32 -0500

    Update 1_train_models_synthetic_reg.py

--- analysis/1_train_models_synthetic_reg.py
+++ analysis/1_train_models_synthetic_reg.py
@@ -1,5 +1,5 @@
 """
-Train models on synthetic data.
+Train models on synthetic data, with angle regularization.
 """ 
 import os
 import numpy as np
@@ -9,7 +9,7 @@ from gradient_correction import helper, model_zoo, regul
 
 #------------------------------------------------------------------------
 
-num_trials = 50  
+num_trials = 20  
 model_names = ['cnn_deep', 'cnn_shallow'] 
 activations = ['relu', 'exponential']  
 
@@ -26,35 +26,37 @@ x_train, y_train, x_valid, y_valid, x_test, y_test = helper.load_data(data_path)
 input_shape = x_train.shape[1:]
 output_shape = y_train.shape[1]
 
-#Regularization details 
-reg_factor=2e-2   #  [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
+#Angle regularization details 
+reg_factors = [2e-2, 1e-2, 3e-3, 1e-3, 1e-4, 2e-5, 1e-5, 1e-6, 1e-7]
 
 #------------------------------------------------------------------------
 
+for reg_factor in reg_factors:  
 
-for model_name in model_names:
-    for activation in activations:
-        for trial in range(num_trials):
-            keras.backend.clear_session()
+    for model_name in model_names:
+        for activation in activations:
+            for trial in range(num_trials):
+                keras.backend.clear_session()
             
-            # load model
-            if model_name == 'cnn_deep':
-                model = model_zoo.cnn_deep(input_shape, output_shape, activation=activation)
-            elif model_name == 'cnn_shallow':
-                model = model_zoo.cnn_shallow(input_shape, output_shape, activation=activation)
+                # load model
+                if model_name == 'cnn_deep':
+                    model = model_zoo.cnn_deep(input_shape, output_shape, activation=activation)
+                elif model_name == 'cnn_shallow':
+                    model = model_zoo.cnn_shallow(input_shape, output_shape, activation=activation)
 
-                
-            # set up optimizer/metrics and compile model
-            optimizer = keras.optimizers.Adam(learning_rate=0.001) 
-            loss = keras.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0.0)
-            history, trainer = regul.fit_attr_prior(model, loss, optimizer, x_train, y_train, validation_data=(x_valid, y_valid), verbose=True,  
+
+                # set up optimizer/metrics and compile model
+                optimizer = keras.optimizers.Adam(learning_rate=0.001) 
+                loss = keras.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0.0)
+                history, trainer = regul.fit_attr_prior(model, loss, optimizer, x_train, y_train, validation_data=(x_valid, y_valid), verbose=True,
                                  metrics=['auroc', 'aupr'], num_epochs=100, batch_size=100, shuffle=True, reg_factor=reg_factor,
                                   es_patience=10, es_metric='auroc', es_criterion='max',
                                   lr_decay=0.2, lr_patience=3, lr_metric='auroc', lr_criterion='max')
 
-            # save model
-            base_name = model_name+'_'+activation + '_' + str(reg_factor)
-            name = base_name+'_'+str(trial)
-            weights_path = os.path.join(params_path, name+'.h5')
-            model.save_weights(weights_path)
+                # save model
+                base_name = model_name+'_'+activation + '_' + str(reg_factor)
+                name = base_name+'_'+str(trial)
+                weights_path = os.path.join(params_path, name+'.h5')
+                model.save_weights(weights_path) 
+                
             
