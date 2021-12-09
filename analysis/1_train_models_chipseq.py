7667041cfed603388281c5d16d51d97855dab909
Antonio-Ionaton <antem@bu.edu>
2021-12-09 18:19:35 -0500

    Update 1_train_models_chipseq.py

--- analysis/1_train_models_chipseq.py
+++ analysis/1_train_models_chipseq.py
@@ -10,7 +10,7 @@ from gradient_correction import helper, model_zoo
 #------------------------------------------------------------------------
 
 num_trials = 50  
-model_names = ['cnn_deep'] #'cnn_shallow' 
+model_names = ['cnn_deep', 'cnn_shallow'] # 
 activations = ['relu', 'exponential']  
 
 results_path = helper.make_directory('../results', 'chipseq')  
@@ -18,69 +18,72 @@ params_path = helper.make_directory(results_path, 'model_params')
 
 #------------------------------------------------------------------------
 
-# load data
-experiments = ['CTCF', 'MAX', 'FOXK2', 'Arid3', 'MEIS2', 'GABPA', 'ZNF24', 'LEF1', 'SRF', 'REST']  # Include list of 10 proteins here. 
-filename = experiment + '_200.h5'
-data_path = '../data/' 
-file_path = os.path.join(data_path, filename)
-dataset = h5py.File(file_path, 'r') 
-x_train = np.array(dataset['x_train']).astype(np.float32).transpose([0,2,1])  
-y_train = np.array(dataset['y_train']).astype(np.float32)
-x_valid = np.array(dataset['x_valid']).astype(np.float32).transpose([0,2,1])
-y_valid = np.array(dataset['y_valid']).astype(np.float32)
-x_test = np.array(dataset['x_test']).astype(np.float32).transpose([0,2,1])
-y_test = np.array(dataset['y_test']).astype(np.float32)
-
-# get shapes
-input_shape = x_train.shape[1:]
-output_shape = y_train.shape[1]
-
-#Ensemble version - varied filter size or number of filters 
+#Ensemble version of the experiment - varied filter size or number of filters 
 num_filters= [12,12,12,12,12, 14,14,14,14,14, 16,16,16,16,16, 18,18,18,18,18, 20,20,20,20,20, 22,22,22,22,22, 24,24,24,24,24,26,26,26,26,26,28,28,28,28,28, 30,30,30,30,30]  #num_filters=[12, 14, 16, 18, 20, 22, 24, 26, 28, 30]  #kernel_size=[3, 5, 7, 9,11,13,15,17,19,21]
 
 #------------------------------------------------------------------------
+# load data
+experiments = ['CTCF', 'MAX', 'FOXK2', 'Arid3', 'MEIS2', 'GABPA', 'ZNF24', 'LEF1', 'SRF', 'REST']  # Include list of 10 proteins here. 
 
-for model_name in model_names:
-    for activation in activations:
-        for trial in range(num_trials):
-            keras.backend.clear_session()
+for experiment in experiments: 
+    filename = experiment + '_200.h5'
+    data_path = '../data/' 
+    file_path = os.path.join(data_path, filename)
+    dataset = h5py.File(file_path, 'r') 
+    x_train = np.array(dataset['x_train']).astype(np.float32).transpose([0,2,1])  
+    y_train = np.array(dataset['y_train']).astype(np.float32)
+    x_valid = np.array(dataset['x_valid']).astype(np.float32).transpose([0,2,1])
+    y_valid = np.array(dataset['y_valid']).astype(np.float32)
+    x_test = np.array(dataset['x_test']).astype(np.float32).transpose([0,2,1])
+    y_test = np.array(dataset['y_test']).astype(np.float32)
+
+    # get shapes
+    input_shape = x_train.shape[1:]
+    output_shape = y_train.shape[1]
+
+    #------------------------------------------------------------------------
+    
+    for model_name in model_names:
+        for activation in activations:
+            for trial in range(num_trials):
+                keras.backend.clear_session()
             
-            # load model
-            if model_name == 'cnn_deep':
-                model = model_zoo.cnn_deep(input_shape, output_shape, activation=activation, num_filters=num_filters[trial])
-            elif model_name == 'cnn_shallow':
-                model = model_zoo.cnn_shallow(input_shape, output_shape, activation=activation, num_filters=num_filters[trial])
+                # load model
+                if model_name == 'cnn_deep':
+                    model = model_zoo.cnn_deep(input_shape, output_shape, activation=activation, num_filters=num_filters[trial])
+                elif model_name == 'cnn_shallow':
+                    model = model_zoo.cnn_shallow(input_shape, output_shape, activation=activation, num_filters=num_filters[trial])
 
-            name = experiment +'_'+  model_name+'_'+activation+'_'+str(trial)
-            print('model: ' + name)
+                name = experiment +'_'+  model_name+'_'+activation+'_'+str(trial)
+                print('model: ' + name)
 
-            # set up optimizer/metrics and compile model
-            auroc = keras.metrics.AUC(curve='ROC', name='auroc')
-            optimizer = keras.optimizers.Adam(learning_rate=0.001)
-            loss = keras.losses.BinaryCrossentropy(from_logits=False)
-            model.compile(optimizer=optimizer, loss=loss, metrics=[auroc])
-            print(model.summary())
-            # setup callbacks
-            es_callback = keras.callbacks.EarlyStopping(monitor='val_auroc', 
+                # set up optimizer/metrics and compile model
+                auroc = keras.metrics.AUC(curve='ROC', name='auroc')
+                optimizer = keras.optimizers.Adam(learning_rate=0.001)
+                loss = keras.losses.BinaryCrossentropy(from_logits=False)
+                model.compile(optimizer=optimizer, loss=loss, metrics=[auroc])
+                print(model.summary())
+                # setup callbacks
+                es_callback = keras.callbacks.EarlyStopping(monitor='val_auroc', 
                                                         patience=10, 
                                                         verbose=1, 
                                                         mode='max', 
                                                         restore_best_weights=True)
-            reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_auroc', 
+                reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_auroc', 
                                                           factor=0.2,
                                                           patience=3, 
                                                           min_lr=1e-7,
                                                           mode='max',
                                                           verbose=1) 
 
-            # fit model
-            history = model.fit(x_train, y_train, 
+                # fit model
+                history = model.fit(x_train, y_train, 
                                 epochs=100,
                                 batch_size=100, 
                                 shuffle=True,
                                 validation_data=(x_valid, y_valid), 
                                 callbacks=[es_callback, reduce_lr])
 
-            # save model
-            weights_path = os.path.join(params_path, name+'.h5')
-            model.save_weights(weights_path)
+                # save model
+                weights_path = os.path.join(params_path, name+'.h5')
+                model.save_weights(weights_path)
