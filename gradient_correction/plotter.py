15d52647ba8ba1c74239a3f195e2dd18594dd95b
Antonio-Ionaton <antem@bu.edu>
2021-12-10 00:25:47 -0500

    Update plotter.py

--- gradient_correction/plotter.py
+++ gradient_correction/plotter.py
@@ -186,3 +186,36 @@ def plot_regularization_angles(log_reg, Angles_std, y_min, y_max, labelsize=20, 
     ax1.grid(alpha=.4)
 
     plt.show()
+
+    
+def plot_initialization(experiment_name,log_reg, Performance, Cosine, Performance_std, Cosine_std, y1_min, y1_max, y2_min, y2_max, labelsize=20, fontsize=25):
+    # Plot Line1 (Left Y Axis)
+    fig, ax1 = plt.subplots(1,1,figsize=(12,7), dpi= 80)
+    ax1.plot(log_reg, Performance, color='tab:red', linewidth=5.0)
+    ax1.scatter(log_reg, Performance, color='tab:red', s=100)
+    ax1.fill_between(log_reg, np.array(Performance)-np.array(Performance_std), np.array(Performance)+np.array(Performance_std), color="r", alpha=0.2)
+    ax1.set_ylim(y1_min,y1_max)
+
+    # Plot Line2 (Right Y Axis)
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.plot(log_reg, Cosine, color='tab:blue', linewidth=5.0)
+    ax2.scatter(log_reg, Cosine, color='tab:blue', s=100)
+    ax2.fill_between(log_reg, np.array(Cosine)-np.array(Cosine_std), np.array(Cosine)+np.array(Cosine_std), color="b", alpha=0.2)
+    ax2.set_ylim(y2_min,y2_max)
+
+    # Decorations
+    # ax1 (left Y axis)
+    ax1.set_xlabel('Initialization standard deviation', fontsize=fontsize)
+    ax1.tick_params(axis='x', rotation=0, labelsize=labelsize)
+    ax1.set_ylabel('Classification AUC', color='tab:red', fontsize=fontsize)
+    ax1.tick_params(axis='y', rotation=0, labelcolor='tab:red' , labelsize=labelsize)
+    ax1.grid(alpha=.4)
+
+    # ax2 (right Y axis)
+    ax2.set_ylabel("Angle std (degrees)", color='tab:blue', fontsize=fontsize)
+    ax2.tick_params(axis='y', labelcolor='tab:blue', labelsize=labelsize)
+    ax2.set_title(experiment_name, fontsize=fontsize)
+    fig.tight_layout()
+
+
+    plt.show()    
