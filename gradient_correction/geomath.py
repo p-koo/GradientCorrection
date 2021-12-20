c71c17243689c0862754b0a616e743ef57b1f133
Antonio-Ionaton <antem@bu.edu>
2021-12-20 02:59:58 -0500

    Update geomath.py

--- gradient_correction/geomath.py
+++ gradient_correction/geomath.py
@@ -38,6 +38,22 @@ def dispersion(saliency_score, saliency_score_ensemble):
   return L2(saliency_score - saliency_score_ensemble) 
 
 
+def dispersion_bins_func(experiments, dispersion, sine_flattened):
+    dispersion_bins={}
+    dispersion_bins_corrected={}
+    for experiment in experiments:  
+        dispersion_flat = dispersion[experiment].reshape(-1) 
+        sine_bins = np.arange(-90,91,5).astype(float)
+        dispersion_bins_sum =  (sine_bins * 0).astype(float)
+        dispersion_bins_count = (sine_bins * 0).astype(float)
+        for i in range (0, len(sine_flattened[experiment])):
+            bin = ((sine_flattened[experiment][i]+90)/180 * len(sine_bins)).astype(int)
+            if(not np.isnan(sine_flattened[experiment][i]) ): # removing problematic resuls from NA and similar. 
+                dispersion_bins_sum[bin]+=dispersion_flat[i] 
+                dispersion_bins_count[bin]+=1
+        dispersion_bins[experiment] = dispersion_bins_sum / dispersion_bins_count
+    return dispersion_bins 
+
 
 def count_large_angles(sine): 
     def frac_larger_angles(sine, threshold=30): 
